b906a3b055694bbc2063b8db4698bf0a68bb0e69
Gal R <gal8156@gmail.com>
2025-10-07 15:17:04 +0300

    bootstrap path fix

--- usage/bootstrap/main.tf
+++ usage/bootstrap/main.tf
@@ -1,8 +1,8 @@
 module "tf_backend" {
-  source       = "../modules/tf-backend"
+  source       = "../modules/tf-backend"   # was ../modules/...
   state_bucket = "backend-tf-state-021891580761-eu-central-1-eks-deployment"
   lock_table   = "terraform-locks"
-  tags = { 
+  tags = {
     owner  = "gal"
     scope  = "observability"
     project= "prometheus"
